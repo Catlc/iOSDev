f02090e7fd10ab5eb5c1655de72b23db6d60f002
李承 <you@example.com>
2017-05-09 09:56:24 +0800

    Initial Commit

---
+++ iOSDev/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSDev
+//
+//  Created by 李承 on 2017/5/9.
+//  Copyright © 2017年 李承. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ iOSDev/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSDev
+//
+//  Created by 李承 on 2017/5/9.
+//  Copyright © 2017年 李承. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
